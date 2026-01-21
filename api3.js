115f859908c96357ae068471ac412aac10b907d9
kmsmahi <salauddinmahi2002@gmail.com>
2026-01-21 10:30:19 +0600

    third

--- api3.js
+++ api3.js
@@ -4,6 +4,12 @@ const loadData=()=>{
     .then(data=>displaydata(data.categories))
     .error(err=>console.log(err))
 };
+const loadFoodsBycategory=(id)=>{
+    fetch(` https://taxi-kitchen-api.vercel.app/api/v1/categories/${id}`)
+    .then((res)=>res.json())
+    .then((data)=>displayFoodsBycategory(data.foods))
+    .error(err=>console.log(err))
+};
 const displaydata=(categories)=>{
     const categoryContainer=document.getElementById('category-container');
     console.log(categoryContainer);
@@ -11,7 +17,7 @@ const displaydata=(categories)=>{
     categories.forEach(category=>{
         const categoryCard=document.createElement('div');
         categoryCard.innerHTML=`
-        <button class="btn justify-start btn-block shadow btn-category">
+        <button onclick="loadFoodsBycategory('${category.id}')" class="btn justify-start btn-block shadow btn-category">
             <img
               src="${category.categoryImg}"
               alt=""
@@ -22,5 +28,42 @@ const displaydata=(categories)=>{
         categoryContainer.appendChild(categoryCard);
     })
 
+}
+const displayFoodsBycategory=(foods)=>{
+    const foodContainer=document.getElementById('food-container');
+    foodContainer.innerHTML='';
+    foods.forEach(food=>{
+        const foodCard=document.createElement('div');
+        foodCard.innerHTML=`
+        <div class="p-5 bg-white flex gap-3 shadow rounded-xl">
+            <div class="img flex-1">
+              <img
+                src="${food.foodImg}"
+                alt=""
+                class="w-[160px] rounded-xl h-[160px] object-cover"
+              />
+            </div>
+            <div class="flex-2">
+              <h1 class="text-xl font-bold">
+                ${food.title}
+              </h1>
+
+              <div class="badge badge-warning">${food.category}</div>
+
+              <div class="divider divider-end">
+                <h2 class="text-yellow-600 font-semibold">
+                  $ <span class="price">${food.price}</span> BDT
+                </h2>
+              </div>
+
+              <button class="btn btn-warning">
+                <i class="fa-solid fa-square-plus"></i>
+                Add This Item
+              </button>
+            </div>
+          </div>
+        `
+        foodContainer.appendChild(foodCard);
+    })
 }
 loadData();
